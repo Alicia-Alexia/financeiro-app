7911078b40368b5328d0dea8c9a06d43e9d32331
Alicia <alicia.alexia03@gmail.com>
2025-07-29 23:16:55 -0300

    feat(pagamento): Configura Redux Store com pagamentosSlice

--- front-financeiro/src/store/index.ts
+++ front-financeiro/src/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import despesaReducer from '../features/despesaSlice';
 import empenhoReducer from '../features/empenhoSlice';
+import pagamentoReducer  from '../features/pagamentoSlice';
 
 
 export const store = configureStore({
   reducer: {
     despesa: despesaReducer,
     empenho: empenhoReducer,
+    pagamento: pagamentoReducer,
   },
 });
 
